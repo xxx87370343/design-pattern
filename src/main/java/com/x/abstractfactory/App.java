293e9ec7d4c43a681328001f06469d8edb9176fe
Liyun Xu <liyun@edmodo.com>
2020-12-25 18:41:03 +0800

    abstract-factory: 引入简单工厂，通过枚举参数决定创建哪个工厂

--- src/main/java/com/x/abstractfactory/App.java
+++ src/main/java/com/x/abstractfactory/App.java
@@ -2,6 +2,7 @@ package com.x.abstractfactory;
 
 import com.x.abstractfactory.factory.ArmyFactory;
 import com.x.abstractfactory.factory.impl.ElfArmyFactory;
+import com.x.abstractfactory.factory.impl.FactoryMaker;
 import com.x.abstractfactory.factory.impl.GodArmyFactory;
 import com.x.abstractfactory.factory.impl.OrcArmyFactory;
 import com.x.abstractfactory.product.Archer;
@@ -11,31 +12,30 @@ import com.x.abstractfactory.product.Saber;
 public class App {
     public static void main(String[] args) {
         ArmyFactory orcArmyFactory = new OrcArmyFactory();
-        Lancer orcLancer = orcArmyFactory.createLancer();
-        Saber orcSaber = orcArmyFactory.createSaber();
-        Archer orcArcher = orcArmyFactory.createArcher();
-        System.out.println(orcLancer.getDescription());
-        System.out.println(orcSaber.getDescription());
-        System.out.println(orcArcher.getDescription());
+        outPutArmy(orcArmyFactory);
 
         System.out.println("====================================");
 
         ArmyFactory elfArmyFactory = new ElfArmyFactory();
-        Lancer elfLancer = elfArmyFactory.createLancer();
-        Saber elfSaber = elfArmyFactory.createSaber();
-        Archer elfArcher = elfArmyFactory.createArcher();
-        System.out.println(elfLancer.getDescription());
-        System.out.println(elfSaber.getDescription());
-        System.out.println(elfArcher.getDescription());
+        outPutArmy(elfArmyFactory);
 
         System.out.println("====================================");
 
         ArmyFactory godArmyFactory = new GodArmyFactory();
-        Lancer godLancer = godArmyFactory.createLancer();
-        Saber godSaber = godArmyFactory.createSaber();
-        Archer godArcher = godArmyFactory.createArcher();
-        System.out.println(godLancer.getDescription());
-        System.out.println(godSaber.getDescription());
-        System.out.println(godArcher.getDescription());
+        outPutArmy(godArmyFactory);
+
+        System.out.println("====================================");
+
+        ArmyFactory godArmyFactory2 = FactoryMaker.getArmyFactory(FactoryMaker.FactoryType.GOD);
+        outPutArmy(godArmyFactory2);
+    }
+
+    private static void outPutArmy(ArmyFactory armyFactory) {
+        Lancer lancer = armyFactory.createLancer();
+        Saber saber = armyFactory.createSaber();
+        Archer archer = armyFactory.createArcher();
+        System.out.println(lancer.getDescription());
+        System.out.println(saber.getDescription());
+        System.out.println(archer.getDescription());
     }
 }
